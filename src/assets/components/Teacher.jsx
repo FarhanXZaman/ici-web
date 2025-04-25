05734349f7a0f0141f6ab6fb98b52cbbd8ccc67d
FarhanXZaman <zamanfarhan3@gmail.com>
2025-04-26 00:17:53 +0600

    set

--- src/assets/components/Teacher.jsx
+++ src/assets/components/Teacher.jsx
@@ -5,7 +5,7 @@ export default function Teacher(props) {
   const{teacher, desp} = props;
   return (
     <>
-        <div className='w-auto h-56 mx-4 px-3 pt-6 gap-6 flex justify-between bg-white rounded-2xl border-2'>
+        <div className='w-auto h-max mx-4 px-3 pt-6 gap-6 flex justify-between bg-white rounded-2xl border-2'>
             <img className='w-28 h-40 mt-2 object-cover rounded-2xl'  src={teacher} alt="" />
             
                 <p className="details text-black text-[13px] font-mono w-56">{desp}</p>
